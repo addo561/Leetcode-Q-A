fa4a472121110c8dc9574d9cde3c770118abcc06
Larry122445 <korlilarryaddo43@gmail.com>
2024-10-09 11:49:13 +0000

    new

---
+++ Arrays/is_subsequence.py
@@ -0,0 +1,10 @@
+link = 'https://leetcode.com/problems/is-subsequence/description/'
+
+
+def is_subsequence(s,t):
+    i = j = 0
+    while i < len(s) and j < len(t):
+        if s[i] == t[j]:
+            i += 1
+        j += 1
+    return i == len(s)        
