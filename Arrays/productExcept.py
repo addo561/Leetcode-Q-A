a7fd4d4e71ec73bbc907bdfe73fb716adc8bf47f
Larry122445 <korlilarryaddo43@gmail.com>
2024-10-29 15:49:08 +0000

    new

---
+++ Arrays/productExcept.py
@@ -0,0 +1,22 @@
+from functools import reduce
+from typing import List
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        prefix = [0] * n
+        suffix = [0] * n
+        product = [0] * n
+
+        prefix[0] = 1
+        for i in range(1,n):
+            prefix[i] = nums[i - 1] * prefix[i - 1]
+
+        suffix[n-1] = 1
+        for i in range(n-2,-1,-1):
+            suffix[i] = nums[i + 1] * suffix[i + 1]
+
+        for i in range(n):
+            product[i] = suffix[i] * prefix[i]
+
+
+        return product    
